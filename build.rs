91f470e79b01d9880877784601e7a938a84e9e1d
Anthony Moi <m.anthony.moi@gmail.com>
2021-09-02 16:55:40 +0200

    Try adding the static_crt flag

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag("-std=c++11")
+        .static_crt(true)
         .file("src/esaxx.cpp")
         .include("src")
         .compile("esaxx");
